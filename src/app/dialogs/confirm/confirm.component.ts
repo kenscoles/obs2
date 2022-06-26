ee58b013f431d3991a5a98cb784c668b5dd5367b
Ken Coles <kenscoles@gmail.com>
2022-06-26 22:53:43 +0100

    changed gitignore 26/6

---
+++ src/app/dialogs/confirm/confirm.component.ts
@@ -0,0 +1,11 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ConfirmDialogData } from 'src/app/models/confirm-dialog-data';
+@Component({
+  selector: 'app-confirm',
+  templateUrl: './confirm.component.html',
+  styleUrls: ['./confirm.component.css'],
+})
+export class ConfirmComponent {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: ConfirmDialogData) {}
+}
