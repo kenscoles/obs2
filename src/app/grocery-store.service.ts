78de91b0f695ac6eb1ad97c3b09df4babd8f6f12
Ken Coles <kenscoles@gmail.com>
2024-03-23 15:57:32 +0000

    conversion message

--- src/app/grocery-store.service.ts
+++ src/app/grocery-store.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-
+// used with ITNEXT BehaviorSubject tutorial
+// https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://itnext.io/angular-subjects-8ed5bf7c4f00&ved=2ahUKEwjnyePas4qFAxUuXUEAHYqwAHYQFnoECBAQAQ&usg=AOvVaw1obsG1p0Xrevs94Uh1WmLc
 @Injectable({
   providedIn: 'root'
 })
