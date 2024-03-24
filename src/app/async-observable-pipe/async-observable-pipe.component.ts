0659e6f71051fcba36c7ca2e28de04fe8276f943
Ken Coles <kenscoles@gmail.com>
2024-03-24 15:22:38 +0000

    formatting only

--- src/app/async-observable-pipe/async-observable-pipe.component.ts
+++ src/app/async-observable-pipe/async-observable-pipe.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { Observable, Observer } from 'rxjs';
 import { AsyncPipe, DatePipe } from '@angular/common';
 
 @Component({
     selector: 'async-observable-pipe',
     template: '<div> Time: {{ now | async | date:" dd MMM yy - HH:mm" }}</div>',
+    styles: `:host {
+      display: block;
+      color: white;
+     } `,
     standalone: true,
     imports: [AsyncPipe, DatePipe]
 })
 export class AsyncObservablePipeComponent {
+
   now = new Observable<any>((observer: Observer<any>) => {
     setInterval(() => observer.next(new Date()), 1000);
   });
