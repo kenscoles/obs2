600aa088354a2119c0bce3a1b2420432cfadcadb
Ken Coles <kenscoles@gmail.com>
2024-04-11 14:42:48 +0100

    kb test tweaks

--- src/app/is-even/is-even.component.ts
+++ src/app/is-even/is-even.component.ts
@@ -3,17 +3,21 @@ import { ChangeDetectionStrategy, Component, computed, input } from '@angular/co
 @Component({
   standalone: true,
   selector: 'is-even',
-  template: `<div>Is Even: {{ isEven() }} </div>
-  <div>Is Odd: {{ isOdd() }} </div>
-  @if(isBy10()) {<div>Number divisible by 10 </div>}
-  @if(isBy9()) {<div>Number divisible by 9 </div>}
-  @if(isBy8()) {<div>Number divisible by 8 </div>}
-  @if(isBy7()) {<div>Number divisible by 7 </div>}
-  @if(isBy6()) {<div>Number divisible by 6 </div>}
-  @if(isBy5()) {<div>Number divisible by 5 </div>}
-  @if(isBy4()) {<div>Number divisible by 4 </div>}
-  @if(isBy3()) {<div>Number divisible by 3 </div>}
-  @if(isBy2()) {<div>Number divisible by 2 </div>}
+  template: `
+
+  @if(isOdd()) {<div><strong>{{counter()}}</strong> is an odd number </div>}
+  @if(isEven()) {<div><strong>{{counter()}}</strong> is an even number </div>}
+  @if(isBy12()) {<div>divides by 12 </div>}
+  @if(isBy11()) {<div>divides by 11 </div>}
+  @if(isBy10()) {<div>divides by 10 </div>}
+  @if(isBy9()) {<div>divides by 9 </div>}
+  @if(isBy8()) {<div>divides by 8 </div>}
+  @if(isBy7()) {<div>divides by 7 </div>}
+  @if(isBy6()) {<div>divides by 6 </div>}
+  @if(isBy5()) {<div>divides by 5 </div>}
+  @if(isBy4()) {<div>divides by 4 </div>}
+  @if(isBy3()) {<div>divides by 3 </div>}
+  @if(isBy2()) {<div>divides by 2 </div>}
   `,
    styleUrl: './is-even.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -31,6 +35,8 @@ export class IsEvenComponent {
   isBy8 = computed(() => (this.counter() % 8 == 0) && this.counter()!==0);
   isBy9 = computed(() => (this.counter() % 9 == 0) && this.counter()!==0);
   isBy10 = computed(() => (this.counter() % 10 == 0) && this.counter()!==0);
+  isBy11 = computed(() => (this.counter() % 11 == 0) && this.counter()!==0);
+  isBy12 = computed(() => (this.counter() % 12 == 0) && this.counter()!==0);
 }
 
 
