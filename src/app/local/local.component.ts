ffa1e579b71ea38a3f792c7090e1bdb9e60ce2a3
Ken Coles <kenscoles@gmail.com>
2024-04-25 16:15:40 +0100

    minor tweaks

--- src/app/local/local.component.ts
+++ src/app/local/local.component.ts
@@ -10,19 +10,22 @@ import { LocalService } from '../shared/services/local.service';
 })
 export class LocalComponent {
 localStore = inject(LocalService)
-myData: any = []
+myData: any = [];
+myStored: any = [];
 ngOnInit(): void {
   
 }
 readStorage(){
-  var myId = this.localStore.getData('id')
-  var arr = this.localStore.getData('user')
-  console.log("myId: ", myId)
-
+  var myName = this.localStore.getData('name')
+  this.myStored = JSON.parse(this.localStore.getData('myData')!)
+  console.log(`myName:${myName}`)
+  console.log(`stored data first:${this.myStored.name}`)
+  console.log(`stored data surname:${this.myStored.surname}`)
 }
 writeStorage(){
-this.myData = [{'name':'Ken', 'surname': 'Coles'}]
-
+ var name= "Danny" 
+this.myData = {'name':name, 'surname': 'Coles'}
+this.localStore.saveData('myData',JSON.stringify(this.myData))
   this.localStore.saveData('name', 'Ken')
 }
 }
