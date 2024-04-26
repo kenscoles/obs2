1fe4b5472998120ea66e83ea3e218f6ee0a29d4d
Ken Coles <kenscoles@gmail.com>
2024-04-26 22:06:41 +0100

    sync

--- src/app/local/local.component.ts
+++ src/app/local/local.component.ts
@@ -9,23 +9,23 @@ import { LocalService } from '../shared/services/local.service';
   styleUrl: './local.component.css'
 })
 export class LocalComponent {
-localStore = inject(LocalService)
-myData: any = [];
-myStored: any = [];
-ngOnInit(): void {
-  
-}
-readStorage(){
-  var myName = this.localStore.getData('name')
-  this.myStored = JSON.parse(this.localStore.getData('myData')!)
-  console.log(`myName:${myName}`)
-  console.log(`stored data first:${this.myStored.name}`)
-  console.log(`stored data surname:${this.myStored.surname}`)
-}
-writeStorage(){
- var name= "Danny" 
-this.myData = {'name':name, 'surname': 'Coles'}
-this.localStore.saveData('myData',JSON.stringify(this.myData))
-  this.localStore.saveData('name', 'Ken')
-}
+  localStore = inject(LocalService)
+  myData: any = [];
+  myStored: any = [];
+  ngOnInit(): void {
+
+  }
+  readStorage() {
+    var myName = this.localStore.getData('name')
+    this.myStored = JSON.parse(this.localStore.getData('myData')!)
+    console.log(`myName:${myName}`)
+    console.log(`stored data first:${this.myStored.name}`)
+    console.log(`stored data surname:${this.myStored.surname}`)
+  }
+  writeStorage() {
+    var name = "Danny"
+    this.myData = { 'name': name, 'surname': 'Coles' }
+    this.localStore.saveData('myData', JSON.stringify(this.myData))
+    this.localStore.saveData('name', 'Ken')
+  }
 }
