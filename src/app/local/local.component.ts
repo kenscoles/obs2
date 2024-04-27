e3706f9ce399d237cf9d8b79469574285166be9d
Ken Coles <kenscoles@gmail.com>
2024-04-27 23:26:50 +0100

    display stored data

--- src/app/local/local.component.ts
+++ src/app/local/local.component.ts
@@ -1,31 +1,49 @@
 import { Component, inject } from '@angular/core';
 import { LocalService } from '../shared/services/local.service';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-local',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, MatButtonModule, MatInputModule, MatSelectModule],
   templateUrl: './local.component.html',
   styleUrl: './local.component.css'
 })
 export class LocalComponent {
-  localStore = inject(LocalService)
-  myData: any = [];
-  myStored: any = [];
-  ngOnInit(): void {
 
-  }
-  readStorage() {
-    var myName = this.localStore.getData('name')
+  localStore = inject(LocalService)
+  fb = inject(FormBuilder)
+
+  myForm = this.fb.group({
+    firstName: [null, Validators.required],
+    lastName: [null, Validators.required],
+    colour: [null, Validators.required],
+    lastUsed: ""
+  });
+
+//  myData: any = [];
+  myStored: any = [];
+  used = ""
+
+  ngOnInit(): void { // read in stored values
     this.myStored = JSON.parse(this.localStore.getData('myData')!)
-    console.log(`myName:${myName}`)
-    console.log(`stored data first:${this.myStored.name}`)
-    console.log(`stored data surname:${this.myStored.surname}`)
+    this.used = JSON.parse(this.localStore.getData('lastUsed')!)
+    this.myForm.patchValue({
+      firstName: this.myStored.firstName,
+      lastName: this.myStored.lastName,
+      colour: this.myStored.colour,
+      lastUsed: this.used
+    })
+var myTime = Date()
+console.log("Date:", myTime) 
   }
-  writeStorage() {
-    var name = "Danny"
-    this.myData = { 'name': name, 'surname': 'Coles' }
-    this.localStore.saveData('myData', JSON.stringify(this.myData))
-    this.localStore.saveData('name', 'Ken')
+
+  onSubmit() {
+    console.log(this.myForm.value)
+    this.localStore.saveData('myData', JSON.stringify(this.myForm.value))
+    this.localStore.saveData('lastUsed', JSON.stringify(Date()))
   }
 }
