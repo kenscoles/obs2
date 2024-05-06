ec3d28590311b73a443bde6d4fcd600c65eec603
Ken Coles <kenscoles@gmail.com>
2024-05-06 23:06:11 +0100

    changes in test1

--- src/app/debounce/debounce.component.ts
+++ src/app/debounce/debounce.component.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 import { DebounceClickDirective } from '../shared/directives/debounce-click.directive';
 import { DebounceKeyupDirective } from '../shared/directives/debounce-keyup.directive';
 import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-debounce',
   standalone: true,
-  imports: [DebounceClickDirective, DebounceKeyupDirective, CommonModule],
+  imports: [DebounceClickDirective, DebounceKeyupDirective, CommonModule,
+  MatButtonModule, MatInputModule, MatSelectModule],
   templateUrl: './debounce.component.html',
   styleUrl: './debounce.component.css'
 })
